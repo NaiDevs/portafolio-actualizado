9dab8eea66f2997dbf847d840f1c11ace0c56b0f
Naidelyn Maldonado <naidelynmaldonado54@gmail.com>
2023-11-02 22:38:38 -0600

    creacion

---
+++ portafolio-actualizado-Naidelyn/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import * as glob from 'glob';
+import path, { resolve } from 'node:path';
+import { ViteMinifyPlugin} from 'vite-plugin-minify'
+import htmlPurge from 'vite-plugin-purgecss';
+import handlebars from 'vite-plugin-handlebars';
+
+import handlerBarsContext from './variables.js';
+
+export default defineConfig({
+    base: "/pw2_github_deploy_vite_static/",
+    appType: 'mpa',
+    build: {
+        rollupOptions: {
+            input: Object.fromEntries(
+                [...glob.sync('./!(dist)/*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ]),
+                ...glob.sync('./*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ])]
+            ),
+        },
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'partials'),
+            context: (pagePath) => {
+                return handlerBarsContext[pagePath];
+            },
+        }),
+        htmlPurge({}),
+        ViteMinifyPlugin({}),
+    ]
+})
